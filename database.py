73dd5f3610957f6e93879fba37f185d14bbd78f2
Павел Дровнин <pavel98rus@gmail.com>
2023-02-05 04:04:27 +0300

    add readme

--- database.py
+++ database.py
@@ -1,20 +1,19 @@
 import sqlite3
 
-conn = sqlite3.connect(
-	'data.db', check_same_thread=False
-)
+conn = sqlite3.connect('data.db', check_same_thread=False)
 cursor = conn.cursor()
 
 
 def db_table_val(
-	user_id: int, user_name: str, user_surname: str, username: str
+    user_id: int, user_name: str, user_surname: str, username: str
 ) -> bool:
-	"""Добавление пользователя в базу"""
-	info = cursor.execute('SELECT * FROM users WHERE user_id=?', (user_id,))
-	if info.fetchone() is None:
-		cursor.execute(
-			'INSERT INTO Users (user_id, user_name, user_surname, username) '
-			'VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username))
-		conn.commit()
-		print(cursor.fetchall())
-		return True
+    """Добавление пользователя в базу"""
+    info = cursor.execute('SELECT * FROM users WHERE user_id=?', (user_id,))
+    if info.fetchone() is None:
+        cursor.execute(
+            'INSERT INTO Users (user_id, user_name, user_surname, username) '
+            'VALUES (?, ?, ?, ?)', (user_id, user_name, user_surname, username)
+        )
+        conn.commit()
+        print(cursor.fetchall())
+        return True
